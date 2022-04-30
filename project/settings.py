d0f38db7a5a70fddf6d62b586fd6e7449b43293a
Didackmon Pyshis <diegorpro2024@gmail.com>
2022-04-30 01:06:09 -0500

    Beta 001 No database

--- project/settings.py
+++ project/settings.py
@@ -68,16 +68,18 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if DEBUG:
+if DEBUG == False:
+    db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    DATABASES['default'].update(db_from_env)
+
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-    DATABASES['default'].update(db_from_env)
+    
 
 
 # Password validation
