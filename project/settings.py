550c5be477055ba002f4010f1fde2c050c94cdcb
Didackmon Pyshis <diegorpro2024@gmail.com>
2022-05-04 23:01:31 -0500

    Beta001

--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
-
+print(DEBUG)
 ALLOWED_HOSTS = ["utilesescolarespapeleriapyshis.herokuapp.com", "localhost"]
 
 
