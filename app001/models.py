d39141e05ea16349fb148474059d11453126513f
Didackmon Pyshis <diegorpro2024@gmail.com>
2022-05-14 16:09:51 -0500

    Beta V1

--- app001/models.py
+++ app001/models.py
@@ -3,9 +3,12 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to="product_img")
+    image = models.CharField(max_length=200)
     quantity = models.PositiveSmallIntegerField(default=0)
+    value_to_yoli = models.DecimalField(max_digits=8, decimal_places=2)
+    earnings = models.DecimalField(max_digits=8, decimal_places=2)
     stock = models.BooleanField(default=True)
+    show_price = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
